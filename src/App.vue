a4aa3389bd88da1dbc37dbbddc4d5e8821544fd2
joaogmotaa <joaogabrielceeduc2020@gmail.com>
2025-04-22 11:59:56 -0300

    ate a segunda

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <body>
     <header>
       <div class="container">
         <a href="#">
@@ -34,10 +35,52 @@
       </div>
     </header>
     <main>
-    <div class="sobre"></div>
-    <div class="imagem_sobre"></div>
+      <div class="linhaAntes"></div>
+      <section class="sobre1">
+      <div class="sobre">
+        <h3>Mesa de Peroba Rosa <br> circular pé de aranha</h3>
+        <ul>
+          <li>
+          </li>
+          <li>
+            <p class="textosafado">
+              Feita em peroba rosa de demolição, essa mesa tem estilo e resistência. O pé de <br> aranha garante firmeza e um visual diferente do comum, com aquele toque retrô <br> que chama atenção sem exagero.
+              Perfeita pra quem curte madeira de verdade e <br> quer uma peça durável com personalidade.
+              Sem firula, só madeira boa e um design <br> que funciona.
+        </p>
+        </li>
+        <li>
+          <p class="medidas">
+            Medidas médias:
+            Diâmetro: 1,20m
+            Altura: 75cm
+          </p>
+        </li>
+        <li>
+          <button class="botao">Acessar pagina de acesso</button>
+        </li>
+      </ul>
+    </div>
+    <div class="imagem_sobre"><img
+        src="https://dcdn-us.mitiendanube.com/stores/003/118/708/products/f6af9f8750d144fc661558489ac22504-a148db03f22c7600d716885562954946-1024-1024.png"
+        alt="icone" class="icon"></div>
+        <div class="butao">
+        </div>
+      </section>
+      <div class="linha"></div>
+      <div class="segundaParte">
+        <div class="imagensDois">
+          <img src="@/assets/livro.png" alt="livro">
+          <p>Frete grátis para SC</p>
+          <img src="@/assets/Star.png" alt="estrela">
+          <p>Livros recomendados</p>
+          <img src="@/assets/Truck.png" alt="caminhaum">
+          <p>Mais vendidos</p>
+        </div>
+        <div class="linhaDois"></div>
+      </div>
     </main>
- 
+  </body>
 </template>
 
 <style scoped>
@@ -176,7 +219,6 @@ table {
   border-collapse: collapse;
   border-spacing: 0;
 }
-
 header {
   font-family: "Oswald";
 } 
@@ -188,7 +230,7 @@ header {
 }
 
 .container h1 {
-  margin-top: 0.5vh;
+  margin-top: 1.5vh;
   font-size: x-large;
   color: #441f05;
 }
@@ -225,11 +267,12 @@ header {
 }
 .coisas {
   padding-left: 4vw;
-  margin-top: 2vh;
+  margin-top: 1vh;
   display: flex;
 }
 
 .coisas a {
+  margin-top: 1.5vh;
   padding-right: 2.7vw;
   text-decoration: none;
   color: #8B4F24;
@@ -238,7 +281,7 @@ header {
 .pesquisar img {
   position: absolute;
   right: 0.5vw;
-  top: 4.2vh;
+  top: 3.8vh;
   transform: translateY(-50%);
   width: 1.2vw;
   height: auto;
@@ -248,16 +291,85 @@ header {
 }
 .coisas .img_header img {
   padding-left: 1.5vw;
-  margin-top: 1vh;
+  margin-top: 1.5vh;
   width: 1.5vw;
   height: 3vh;
 }
 .divididorDois {
   margin-left: 1vw;
-  margin-top: 1.4vh;
+  margin-top: 1.9vh;
   width: 2px;
   height: 2vh;
   background-color: #8B4F24;
   border-radius: 15px;
+}
+.sobre1 {
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  padding: 40px;
+}
+.sobre {
+  margin-left: 10vw;
+}
+.sobre h3 {
+  font-weight: bold;
+  font-size: xx-large;
+  padding:0 0 1.5vw 0 ;
+}
+
+.imagem_sobre img{
+  width: 30vw;
+  margin-right: 10vw ;
+}
+.medidas{
+  padding: 1vw 0 0 0;
+  font-size: medium ;
+  font-weight: bold;
+}
+.textosafado {
+  font-size: medium;
+}
+.botao {
+  font-size: x-large;
+  background-color: #8B4F24;
+  border-radius: 5px;
+  padding: 20px;
+  color: white;
+  margin: 2.5vw 0 0 0;
+}
+
+.linhaAntes {
+  background-color: #8B4F24;
+  margin-left: 10vw;
+  margin-top: 2vh;
+  width: 80vw;
+  height: 0.1vh;
+}
+.linha{
+  background-color: #8B4F24;
+  margin-left: 10vw;
+  margin-top: 2vh;
+  width: 80vw;
+  height: 0.1vh;
+}
+.linhaDois {
+  background-color: #8B4F24;
+  margin-left: 10vw;
+  margin-top: 2vh;
+  width: 80vw;
+  height: 0.1vh;
+}
+.imagensDois{
+display: flex;
+padding-top: 1vh;
+margin-left: 30vw;
+}
+.imagensDois p{
+  padding-top: 1.5vh;
+  padding-left: 1vw;
+  padding-right: 1vw;
+  font-weight: bold;
+  font-size: larger;
 }
 </style>
